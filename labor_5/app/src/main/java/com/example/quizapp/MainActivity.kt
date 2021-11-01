b39493b15ffea1e90567027f865d09e5323b0183
pad322 <79305213+pad322@users.noreply.github.com>
2021-11-01 19:01:01 +0200

    nem vegso

---
+++ labor_5/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
